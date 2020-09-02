5be8e6ce03baee2f63741cee9a494034d51cf325
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2020-09-02 10:57:16 +0200

    add docstring

--- src/processing_utils/image_stats_calculator.py
+++ src/processing_utils/image_stats_calculator.py
@@ -2,5 +2,9 @@ from .rate_calculator import RateCalculator
 
 
 class ImageStatsCalculator(RateCalculator):
+    """
+    Utility to calculate average of any image related parameter, and throttle
+    its refresh to the desired interval
+    """
     def update(self, value):
         self.counter += value
