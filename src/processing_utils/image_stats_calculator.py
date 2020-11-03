56ee54085710e2beb6fc9fefdf40a3d2253c6c5c
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2020-11-03 13:33:49 +0100

    option to average image stats over time

--- src/processing_utils/image_stats_calculator.py
+++ src/processing_utils/image_stats_calculator.py
@@ -33,11 +33,16 @@ class ImageStatsCalculator:
         else:
             return 0.
 
-    def refresh(self):
+    def refresh(self, do_time_average=False):
         """If refresh interval has elapsed, it resets sum_value and elapsed
-        time, and returns the averaged value. Otherwise, it returns None"""
+        time, and returns the averaged value. Otherwise, it returns None.
+        If do_time_average is set to True, the time average is returned.
+        """
         val = None
         if self.elapsedTime() >= self.refresh_interval:
-            val = self.get_average_value()
+            if do_time_average:
+                val = self.sum_value / self.elapsedTime()
+            else:
+                val = self.get_average_value()
             self.reset()
         return val
