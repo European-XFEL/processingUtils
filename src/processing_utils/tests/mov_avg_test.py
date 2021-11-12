8db9a55f4d6846c65d51559f72512c780b1766ae
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2021-11-12 10:38:24 +0100

    moving average unit test

---
+++ src/processing_utils/tests/mov_avg_test.py
@@ -0,0 +1,33 @@
+import unittest
+
+import random
+
+from time import sleep, time
+
+from ..moving_average import MovingAverage
+
+AVG_N = 10
+
+class ImageStatsCalculator_TestCase(unittest.TestCase):
+    mov_avg = MovingAverage(AVG_N)
+
+    def test_moving_avg(self):
+        i = 0
+        sum = 0
+        while True:
+            i += 1
+            if i < AVG_N:
+                sum += i
+                val, n = self.mov_avg(i)
+                self.assertEqual(val, sum / i)
+                self.assertEqual(n, i)
+            elif i <= AVG_N + 10:
+                sum = sum + i - (i - AVG_N)
+                val, n = self.mov_avg(i)
+                self.assertEqual(val, sum / AVG_N)
+                self.assertEqual(n, 10)
+            else:
+                break
+
+if __name__ == '__main__':
+    unittest.main()
