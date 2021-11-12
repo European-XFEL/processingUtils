0de9d61a5c46ae01fe030a97826dd93c577bc065
Gabriele Giovanetti <gabriele.giovanetti@xfel.eu>
2021-11-12 10:23:52 +0100

    added MovingAverage utilty

---
+++ src/processing_utils/moving_average.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class MovingAverage():
+    """
+    Utility for moving average calculation.
+    Example of usage for moving average on N values
+
+    Construction:
+        mov_avg = MovingAverage(N)
+
+    Feeding a value:
+        avg, n = mov_avg(a_value)
+
+           avg is the current average value,
+           n_of_vals is the number of values it is calculated on
+    """
+
+    def __init__(self, windowSize):
+        self.windowSize = windowSize
+        self.window = deque(maxlen=int(windowSize))
+        self.avg = 0
+
+    def __call__(self, value):
+        if value is not None:
+            self.window.append(value)
+            n_of_vals = len(self.window)
+            self.avg = sum(self.window) / n_of_vals
+            return self.avg, n_of_vals
